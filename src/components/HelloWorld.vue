29496cd52708df53597f186587080a1df3cfc6ff
hualvm <cliang772@163.com>
2022-11-22 14:14:03 +0800

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -14,7 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit123
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
